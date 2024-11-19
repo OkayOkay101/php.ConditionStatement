40874433cda90f4e107c6686c6d842d9366bc9f0
OkayOkay101 <78838036+OkayOkay101@users.noreply.github.com>
2024-11-19 09:57:38 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Github</title>
+</head>
+<body>
+    <h1>การใช้คำสั่ง if</h1>
+    <?php
+        echo "<h2?>คำสั่ง if</h2>";
+        $x = 5;
+        $y = 8;
+        echo "\$x = $x <br>";
+        echo "\$y = $y <br>";
+        if($x<$y){
+            echo "x น้อยกว่า y";
+        }
+        else{
+            echo "y น้อยกว่า x";
+        }
+        echo "----------------------------------------";
+        ?>
+        
+
+    <a href="https://www.udru.ac.th">My Info</a>
+
+    
+</body>
+</html>
