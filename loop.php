29a3cd95bbe80943bdfd4253ae07d2ec356d9913
OkayOkay101 <78838036+OkayOkay101@users.noreply.github.com>
2024-11-19 11:37:57 +0700

    Update loop.php

--- loop.php
+++ loop.php
@@ -26,6 +26,53 @@
             echo "do...while รอบที่ $y <br>";
             $y++;
         } while ($y <= 5);
+        echo "-----------------<br>";
+        echo "for loop<br>";
+        for ($i=1; $i <= 10; $i++) { 
+            echo "For รอบที่ $1<br>";
+        }
+        echo "-----------------------------------------<br>";
+
+        
+// Number of rows
+$rows = 5;
+
+// Loop through each row
+for ($i = 1; $i <= $rows; $i++) {
+    // Print stars in each row
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+    // Move to the next line after printing stars for each row
+    echo "<br>";
+}
+
+echo "<br>";
+
+echo "for each looP<br>";
+
+$pet = array("แมว", "สุนัข", "นกแก้ว", "ปลาทอง", "หมูเด้ง");
+
+foreach ($pet as $value) {
+    echo "ฉันชอบ " . $value . "<br>";
+}
+
+echo "---------------<br>";
+
+echo "foreach loop แบบ key=>value<br>";
+
+$food = array(
+    "หมวย" => "ส้มตำ",
+    "หมวย1" => "ส้มตำ1",
+    "หมวย2" => "ส้มตำ2",
+    "หมวย3" => "ส้มตำ3"
+);
+
+foreach ($food as $student => $value) {
+    echo "ชื่อ $student ชอบกิน $value<br>";
+}
+
     ?>
+
 </body>
 </html>
